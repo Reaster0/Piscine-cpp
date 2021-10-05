c218354e3a678248cd04c3f27514c92c0ec3dedc
Evan Reaster <reaster@MacBook-Reaster.local>
2021-10-05 19:20:57 +0200

    end of the zombiehorde

--- day01/ex01/ZombieHorde.cpp
+++ day01/ex01/ZombieHorde.cpp
@@ -8,7 +8,7 @@ void Zombie::change_name(std::string new_name)
 Zombie* zombieHorde(int N, std::string name)
 {
 	Zombie *horde = new Zombie[N];
-	horde[0] = name;
-	horde->announce();
+	for (int i = 0; i < N; i++)
+		horde[i].change_name(name);
 	return horde;
 }
--- day01/ex01/main.cpp
+++ day01/ex01/main.cpp
@@ -1,7 +1,16 @@
 #include "Zombie.hpp"
 
+#define Nbr_Z  4
+
 int main()
 {
-	Zombie *lol = zombieHorde(4, "lolilol");
-	delete [] lol;
+	//allocate the memory for an array of zombie and call them pnj
+	Zombie *boys = zombieHorde(Nbr_Z, "PNJ");
+
+	//make them all tell their names
+	for (int i = 0; i < Nbr_Z; i++)
+		(boys + i)->announce();
+
+	//obvously dont forget to free the memory
+	delete [] boys;
 }
