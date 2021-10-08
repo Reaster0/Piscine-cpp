22ebcf2e0a62a010aead80580ee815efbef540f6
Evan Reaster <reaster@MacBook-Reaster.local>
2021-10-08 23:38:02 +0200

    added day01/ex04

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <climits>
+
+// take a source and replace it with a version where all s1 are remplaced by s2
+void ft_replace(std::string *source, std::string *s1, std::string *s2)
+{
+	std::string result;
+
+	if (!*s1->begin() || !*s2->begin() || !*source->begin())
+		return;
+	while (*source->begin() || source->find(*s1) != ULLONG_MAX)
+	{
+		result += source->substr(0,source->find(*s1));
+		if (source->find(*s1) == ULLONG_MAX)
+			source->erase(0, ULLONG_MAX);
+		else
+		{
+			source->erase(0, source->find(*s1) + (*s1).size());
+			result += *s2;
+		}
+	} 
+	*source = result;
+}
 
 int main(int argc, char **argv)
 {
@@ -12,6 +34,7 @@ int main(int argc, char **argv)
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
 	std::string temp;
+	std::string buffer;
 
 	std::ifstream fileFrom(filename);
 	if (!fileFrom)
@@ -25,8 +48,14 @@ int main(int argc, char **argv)
 		std::cout << "Error: can't write the file\n";
 		return (0);
 	}
-	while (getline(fileFrom, temp))
+	while (getline(fileFrom, buffer))
 	{
-	std::cout << temp << std::endl;
+		temp += buffer;
+		temp.push_back('\n');
 	}
+
+	ft_replace(&temp, &s1, &s2);
+	fileTo << temp;
+	fileTo.close();
+	fileFrom.close();
 }
