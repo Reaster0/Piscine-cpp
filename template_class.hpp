880aa55a511c92932a81fb5d8e317680d397439b
Evan Reaster <reaster@MacBook-Reaster.local>
2021-11-02 14:25:31 +0100

    added template for c++ classes

---
+++ template_class.hpp
@@ -0,0 +1,31 @@
+class Template
+{
+	private:
+
+	public:
+		Template();
+		Template(const Template &other);
+		~Template();
+
+		Template &operator=(const Template &other);
+};
+
+Template::Template()
+{
+
+}
+
+Template::Template(const Template &other)
+{
+
+}
+
+Template::~Template()
+{
+
+}
+
+Template &Template::operator=(const Template &other)
+{
+
+}
