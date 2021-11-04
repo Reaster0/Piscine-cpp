2e45da7e198f5fea109a4688356c4b886e680cc4
Evan Reaster <reaster@MacBook-Reaster.local>
2021-11-04 14:55:53 +0100

    i was dumb, try catch dosen't work like that

--- day05/ex00/Bureaucrat.hpp
+++ day05/ex00/Bureaucrat.hpp
@@ -16,15 +16,26 @@ class Bureaucrat
 		Bureaucrat(std::string const _name, int const note);
 
 		Bureaucrat &operator=(const Bureaucrat &other);
-		void GradeTooHighException();
-		void GradeTooLowException();
 		std::string getName() const;
 		int getGrade() const;
 		void incrementGrade();
 		void decrementGrade();
 
+class GradeTooLowException : public std::exception
+{
+	public :
+	virtual const char *what() const throw(){return "the grade is too low and should not be less than 150";}
+};
+
+class GradeTooHighException : public std::exception
+{
+	public:
+	virtual const char *what() const throw(){return "the grade is too high and should not be over 1";}
+};
+
 };
 
 	std::ostream &operator<<(std::ostream &os, Bureaucrat const &other);
+
 
 #endif
--- day05/ex00/main.cpp
+++ day05/ex00/main.cpp
@@ -5,14 +5,38 @@ int main()
 	Bureaucrat a;
 	Bureaucrat b("lol", 1);
 
-
 	std::cout << a << std::endl;
 
-	Bureaucrat c("lol2", 151); //should throw an error
-	std::cout << c << std::endl;
-	c.decrementGrade(); //should also throw an error
-	std::cout << c << std::endl;
+	try
+	{
+		Bureaucrat c("lol2", 151); //should throw an error
+		std::cout << c << std::endl;
 
-	b.incrementGrade(); //should throw an error
-	std::cout << b << std::endl;
+		c.decrementGrade(); //should also throw an error
+		std::cout << c << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		b.incrementGrade(); //should throw an error
+		std::cout << b << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		b.decrementGrade(); //should work as intended
+		std::cout << b << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 }
