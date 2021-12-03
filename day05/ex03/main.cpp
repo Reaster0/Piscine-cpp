3db52bf4693570e8165bfd8a156ab8c0775bdd31
Evan Reaster <reaster@MacBook-Reaster.local>
2021-12-03 13:25:51 +0100

    ready to release the day05

--- day05/ex03/main.cpp
+++ day05/ex03/main.cpp
@@ -10,14 +10,15 @@ int main()
 	Form *Robot;
 	Form *President;
 	Bureaucrat jean("jean pierre", 4);
-
 	Intern stagiaire;
+	
 	shrub = stagiaire.MakeForm("Shrubbery form", "shrub");
 	try{
 		jean.executeForm(*shrub); //will not work because it's not signed
 	} catch(const std::exception& e){
 		std::cout << e.what() << std::endl;
 	}
+
 	Robot = stagiaire.MakeForm("Robotomy form", "yes man");
 	jean.signForm(*Robot);
 	try{
@@ -25,6 +26,7 @@ int main()
 	} catch(const std::exception& e){
 		std::cout << e.what() << std::endl;
 	}
+
 	President = stagiaire.MakeForm("Presidential form", "the french people");
 	jean.signForm(*President);
 	try{
