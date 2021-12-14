e5ac8c4c7cecc687bf2a3fed06044c3ea0c61771
Evan Reaster <reaster@MacBook-Reaster.local>
2021-12-14 17:00:31 +0100

    an another small modification in iter

--- day07/ex01/iter.hpp
+++ day07/ex01/iter.hpp
@@ -10,4 +10,13 @@ void iter(T1 *arr, T2 len, void (*func)(T1 &thing))
 		func(arr[i++]);
 }
 
+//template<typename T1, typename T2>
+//void iter(T1 *arr, T2 len, void (*func)(const T1 &thing))
+//{
+//	T2 i = 0;
+//	while (i < len)
+//		func(arr[i++]);
+//}  //a version with the function taking t1 as a const
+
+
 #endif
